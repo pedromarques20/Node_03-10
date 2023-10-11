88a8ff651b62fc9f53cc2f833ac7be88d28a0da1
Pedro Marques <pedrohsmarques18@gmail.com>
2023-10-11 09:36:15 -0300

    Aula 03-10

---
+++ projetoninho/app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+// Define a rota para a página HTML
+app.get('/livros', (req, res) => {
+  res.sendFile(__dirname + '/pages/livros/listLivros.html');
+});
+
+// Define a rota para a página HTML
+app.get('/addlivros', (req, res) => {
+  res.sendFile(__dirname + '/pages/livros/livros.html');
+});
+
+// Define a rota para a página HTML
+app.get('/editlivros', (req, res) => {
+  res.sendFile(__dirname + '/pages/livros/editLivro.html');
+});
+
+// Define a rota para a página HTML
+app.get('/categorias', (req, res) => {
+  res.sendFile(__dirname + '/pages/categorias/listCategorias.html');
+});
+
+// Define a rota para a página HTML
+app.get('/addcategorias', (req, res) => {
+  res.sendFile(__dirname + '/pages/categorias/categorias.html');
+});
+
+// Define a rota para a página HTML
+app.get('/editcategorias', (req, res) => {
+  res.sendFile(__dirname + '/pages/categorias/editCategoria.html');
+});
+
+// Define a rota para a página HTML
+app.get('/usuarios', (req, res) => {
+  res.sendFile(__dirname + '/pages/usuarios.html');
+});
+
+// Define a rota para a página HTML
+app.get('/pedidos', (req, res) => {
+  res.sendFile(__dirname + '/pages/pedidos.html');
+});
+
+// Define a rota para a página HTML
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/pages/index.html');
+});
+
+// Inicia o servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}/`);
+});
